367ae2155736656ebe0ab83bdebd1860aba12ca4
Harshal141 <harshalmukundapatil@gmail.com>
2022-10-10 11:30:37 +0530

    contri

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-app.use(express.static('public'))
 app.use('/', express.static("public"))
+app.use('/con',express.static("contributors"))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
